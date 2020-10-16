cdbf57fe6609adbff9f9625f9f215a4c0eead594
lucek3332 <jakubszulgit@gmail.com>
2020-10-16 18:30:01 +0200

    Preparing file structure for website. Creating base template.
    Creating templates folder in project with base template - base.html and homepage - dashboard.html. Creating basic view for homepage. Including account app urls.py in main project urls.py

--- bridge/urls.py
+++ bridge/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from .views import dashboard_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^accounts/', include(("accounts.urls", "accounts"), namespace="accounts")),
+    re_path(r"^$", dashboard_view, name="dashboard"),
 ]
