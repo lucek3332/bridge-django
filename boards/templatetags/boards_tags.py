d92dfacf0b7ca8a2415c1769e749bacda2004e8f
lucek3332 <jakubszulgit@gmail.com>
2020-11-15 14:00:03 +0100

    Writing custom filters. Creating html structure in board detail view.
    Writing filters for modifying bidding and tricks stored in database. Modifying detail.html tempalate for displaying tricks and bidding.

--- boards/templatetags/boards_tags.py
+++ boards/templatetags/boards_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from math import ceil
 
 
 register = template.Library()
@@ -27,5 +28,55 @@ def get_suit(hand, color):
     return res
 
 
+def get_bidding(bidding):
+    bidding = [b.strip(" [''] ") for b in bidding]
+    res = []
+    countRows = ceil(len(bidding) / 4)
+    for row in range(countRows):
+        res.append(bidding[row * 4: row * 4 + 4])
+    return res
+
+
+def get_tricks(tricks):
+    tricks = [t.strip(" [''] ") for t in tricks]
+    modified_tricks = []
+    for t in tricks:
+        if t.endswith("*") and len(t) == 4:
+            if '10' in t:
+                t = 'T' + t[0].lower() + "*"
+            elif '11' in t:
+                t = 'J' + t[0].lower() + "*"
+            elif '12' in t:
+                t = 'Q' + t[0].lower() + "*"
+            elif '13' in t:
+                t = 'K' + t[0].lower() + "*"
+            elif '14' in t:
+                t = 'A' + t[0].lower() + "*"
+        elif t.endswith("*") and len(t) == 3:
+            t = t[1] + t[0].lower() + "*"
+        elif len(t) == 3:
+            if '10' in t:
+                t = 'T' + t[0].lower()
+            elif '11' in t:
+                t = 'J' + t[0].lower()
+            elif '12' in t:
+                t = 'Q' + t[0].lower()
+            elif '13' in t:
+                t = 'K' + t[0].lower()
+            elif '14' in t:
+                t = 'A' + t[0].lower()
+        else:
+            t = t[1] + t[0].lower()
+        modified_tricks.append(t)
+
+    res = []
+    countRows = ceil(len(modified_tricks) / 4)
+    for row in range(countRows):
+        res.append(modified_tricks[row * 4: row * 4 + 4])
+    return res
+
+
 register.filter('get_board_id', get_board_id)
 register.filter('get_suit', get_suit)
+register.filter('get_bidding', get_bidding)
+register.filter('get_tricks', get_tricks)
